3874178102956f462a2a9f7fa68dfc3e54ae2e95
VictGonz <noedelarosaa@gmail.com>
2023-11-24 18:20:32 +0100

    fsdgd
    gsd

--- function.js
+++ function.js
@@ -1,118 +1,75 @@
 
+  let opA;
+  let opB;
+  let operation;
+  let resultDisplay = document.getElementById('result');
+  let one = document.getElementById('n1');
+  let two = document.getElementById('n2');
+  let three = document.getElementById('n3');
+  let four = document.getElementById('n4');
+  let five = document.getElementById('n5');
+  let six = document.getElementById('n6');
+  let seven = document.getElementById('n7');
+  let eight = document.getElementById('n8');
+  let nine = document.getElementById('n9');
+  let zero = document.getElementById('n0');
+  let equal= document.getElementById('equal')
+  let plus= document.getElementById('plus')
+  let minus= document.getElementById('minus')
+  let division = document.getElementById('divide')
+  let multiplication= document.getElementById('multiply')
+  let reset= document.getElementById('clear');
+  let buttons= document.querySelectorAll(".button");
+  console.log(buttons)
+  let buttonValue;
 
-function printNumber() {
 
-var opA;
-var opB;
-var operation;
-let resultDisplay=document.querySelector('Display');
-let n1= document.getElementById('n1');
-let n2 = document.getElementById('n2');
-let n3 = document.getElementById('n3');
-let n4 = document.getElementById('n4');
-let n5 = document.getElementById('n5');
-let n6 = document.getElementById('n6');
-let n7 = document.getElementById('n7');
-let n8 = document.getElementById('n8');
-let n9 = document.getElementById('n9');
-let n0 = document.getElementById('n0');
-let equal= document.getElementById('equal')
-let plus= document.getElementById('plus')
-let minus= document.getElementById('minus')
-let division = document.getElementById('divide')
-let multiplication= document.getElementById('multiply')
-let reset= document.getElementById('clear');
+buttons.forEach(button =>{
+ button.addEventListener("click",printNumber)
+ buttonValue= printNumber.target
+ printNumber.target= Math.floor(printNumber.target)
+ console.log(typeof buttonValue)
+}) 
+/*
 
+one.addEventListener("click",printNumber);
+two.addEventListener("click",printNumber);
+three.addEventListener("click",printNumber);
+four.addEventListener("click",printNumber);
+five.addEventListener("click",printNumber);
+six.addEventListener("click",printNumber);
+seven.addEventListener("click",printNumber);
+eight.addEventListener("click",printNumber);
+nine.addEventListener("click",printNumber);
+zero.addEventListener("click",printNumber);
+*/
+  function printNumber(){
+    
+    console.log("funciona")
+    let resultDisplay = printNumber.target
+   var resultElement = document.getElementById(".result") ;
+   let numberDisplay = resultElement + resultDisplay;
+   numberDisplay.innerHTML
+   console.log(typeof resultElement)
+   console.log( resultElement)
+  }
 
-n1.onclick=function(e){
-  resultDisplay= resultDisplay + "1";
-}
-n2.onclick=function(e){
-  resultDisplay= resultDisplay + "2";
-}
-
-n3.onclick=function(e){
-  resultDisplay= resultDisplay + "3";
-}
-n4.onclick=function(e){
-  resultDisplay= resultDisplay + "4";
-}
-n5.onclick=function(e){
-  resultDisplay= resultDisplay + "5";
-}
-n6.onclick=function(e){
-  resultDisplay= resultDisplay + "6";
-}
-n7.onclick=function(e){
-  resultDisplay= resultDisplay + "7";
-}
-n8.onclick=function(e){
-  resultDisplay= resultDisplay + "8";
-}
-n9.onclick=function(e){
-  resultDisplay= resultDisplay + "9";
-}
-n0.onclick=function(e){
-  resultDisplay.textContent= resultDisplay + "0";
-}
-
-reset.onclick= function(e){
-  reseting();
-}
-plus.onclick = function(e){
- opA = resultDisplay.textContent;
-  operation = "+";
-  clean();
-}
-minus.onclick = function(e){
-  opA = resultDisplay.textContent;
-  operation = "-";
-  clean();
-}
-multiplication.onclick = function(e){
- opA = resultDisplay.textContent;
-  operation = "*";
-  clean();
-}
-division.onclick = function(e){
- opA = resultDisplay.textContent;
-  operation = "/";
-  clean();
-}
-equal.onclick = function(e){
-  opB = resultDisplay.textContent;
-  solve();
-}
-
-function clean(){
-  resultDisplay.textContent= "";
-
-}
-
-function reseting(){
-  resultDisplay.textContent="";
-  opA= "0";
-  opB= "0";
-  operation= "";
-}
-
-function solve(){
-  var final= 0;
-  switch(operation){
-    case "+" :
-    final= parseFloat(opA) + parseFloat(opB);
-    break;
-    case "-":
-    final= parseFloat(opA) - parseFloat(opB);
-    break;
-    case "*":
-    final= parseFloat(opA) * parseFloat(opB);
-    break;
-    case "/":
-    final= parseFloat(opA) / parseFloat(opB);
-    break; 
-  function reseting(){
-    result.textContent= final;
-  }}
-
-  }}
+  function solve(){
+    var final= 0;
+    switch(operation){
+      case "+" :
+      final= parseFloat(opA) + parseFloat(opB);
+      break;
+      case "-":
+      final= parseFloat(opA) - parseFloat(opB);
+      break;
+      case "*":
+      final= parseFloat(opA) * parseFloat(opB);
+      break;
+      case "/":
+      final= parseFloat(opA) / parseFloat(opB);
+      break; 
+    function reseting(){
+      result.textContent= final;
+    }
+  }};
